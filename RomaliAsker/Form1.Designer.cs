46bfb8032e6f4403e315cf62215b3f4758f46f31
Mustafa Aşkın İNAN <mustafaaskininan@gmail.com>
2019-12-22 03:22:21 +0300

    Add files via upload

---
+++ RomaliAsker/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RomaliAsker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKontrol = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblSnc2 = new System.Windows.Forms.Label();
+            this.lblSnc1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdim = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblKontrol
+            // 
+            this.lblKontrol.AutoSize = true;
+            this.lblKontrol.Location = new System.Drawing.Point(10, 154);
+            this.lblKontrol.Name = "lblKontrol";
+            this.lblKontrol.Size = new System.Drawing.Size(0, 13);
+            this.lblKontrol.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(143, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Milliseconds";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.Location = new System.Drawing.Point(172, 113);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(0, 13);
+            this.lblSure.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(115, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Hesaplama Süresi";
+            // 
+            // lblSnc2
+            // 
+            this.lblSnc2.AutoSize = true;
+            this.lblSnc2.Location = new System.Drawing.Point(74, 113);
+            this.lblSnc2.Name = "lblSnc2";
+            this.lblSnc2.Size = new System.Drawing.Size(0, 13);
+            this.lblSnc2.TabIndex = 21;
+            // 
+            // lblSnc1
+            // 
+            this.lblSnc1.AutoSize = true;
+            this.lblSnc1.Location = new System.Drawing.Point(23, 113);
+            this.lblSnc1.Name = "lblSnc1";
+            this.lblSnc1.Size = new System.Drawing.Size(0, 13);
+            this.lblSnc1.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "2.Değer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "1. Değer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Adım";
+            // 
+            // txtAdim
+            // 
+            this.txtAdim.Location = new System.Drawing.Point(47, 38);
+            this.txtAdim.Name = "txtAdim";
+            this.txtAdim.Size = new System.Drawing.Size(160, 20);
+            this.txtAdim.TabIndex = 16;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(9, 64);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(198, 23);
+            this.btnHesapla.TabIndex = 15;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Sayı";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(47, 12);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(160, 20);
+            this.txtSayi.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblKontrol);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblSnc2);
+            this.Controls.Add(this.lblSnc1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdim);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKontrol;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblSnc2;
+        private System.Windows.Forms.Label lblSnc1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdim;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi;
+    }
+}
+
---
+++ RomaliAsker/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RomaliAsker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                
+                    lblSnc1.Text = "";
+                    lblSnc2.Text = "";
+                    lblSure.Text = "";
+                    lblKontrol.Text = "";
+                    int sayi = Convert.ToInt32(txtSayi.Text);
+                    int adim = Convert.ToInt32(txtAdim.Text);
+                    int sayac = sayi;//sayacı sayıya eşitlemekte amaç 2 değer kalana kadar her do while döngüsünde 1 azaltarak 2 kalınca döngüden çıkarmak
+                    int[] dizi = new int[sayi];
+                    int deger = 1;
+
+                    for (int i = 0; i < sayi; i++)//dizi elemanlarına değer ata
+                    {
+                        dizi[i] = deger;
+                        deger++;
+                    }
+
+                    int a = adim - 1;//-1 olmasının nedeni a yı dizilerde kullanacağımız için doğru değer olması için 1 eksik olmalı
+
+                    do
+                    {
+                        int sayac2 = 0;
+                        for (; ; )//bu döngü içinde a nıncı sayının 0 olup olmadığını kontrol ediyoruz eğer 0 sa + değer bulana kadar bir artıyor
+                        {
+                            if (dizi[a] == 0)
+                            {
+                                a++;
+                                if (a > sayi - 1)//a arttığında eğer diziden fazla bi değer olursa dizinin başına dön
+                                {
+                                    a = 0;
+                                }
+                            }
+                            else
+                                break;
+                        }//0 değeri değilse fordan çık
+
+                        if (dizi[a] != 0)
+                        {
+                            dizi[a] = 0;
+
+                            for (int i = a + 1; i < a + adim + sayac2; i++)
+                            {//bu forun içinde a değerini 0 a eşitledikten sonra a'nın 1 sonrasındaki değerden adıma kadar 0 değeri varsa o 0 değerleri adımdan sayılmayacağı için onları sayac2 ye eklemek ve onları daha sonra fazladan adım olarak kullanmak 
+                                int cevap = i;
+
+                                if (cevap > sayi - 1)
+                                {
+                                    cevap = (cevap % sayi); //eğer değer diziden büyükse dizinin başına dönülüyor
+                                }
+                                if (dizi[cevap] == 0)// 0 değeri varsa sayaca ekleniyor
+                                    sayac2++;
+                            }//for sonu
+
+                            a = a + adim + sayac2; // arada 0 değer varsa a'ya adım olarak ekleniyor
+
+                            if (a > sayi - 1)
+                            {
+                                a = (a % sayi);//eğer değer diziden büyükse dizinin başına dönülüyor
+                            }
+                        }
+
+                        sayac--;//baştaki adıma eşit olan sayac 1 azaltılıyor ve 2 olana kadar döngü devam ediyor
+                    } while (sayac != 2);
+
+                    for (int i = 0; i < sayi; i++)
+                    {
+                        if (dizi[i] == 0)
+                            continue;
+                        else
+                        {
+                            if (lblSnc1.Text == "")
+                            {
+                                lblSnc1.Text = Convert.ToString(dizi[i]);
+                            }
+                            else
+                                lblSnc2.Text = Convert.ToString(dizi[i]);
+                        }
+                    }
+
+                
+                watch.Stop();
+                lblSure.Text = Convert.ToString(watch.Elapsed.Milliseconds);
+            }//try sonu
+
+            catch (Exception)
+            {   
+                lblKontrol.Text = "Geçerli Değer Giriniz...";   
+            }
+        }
+    }
+}
